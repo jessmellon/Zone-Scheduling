3953afa3370f6e29bf606a562fe06599daf1d9c4
jessmellon <jessica@vosphoto.com>
2026-03-23 12:00:27 -0400

    Removed the word "Scheduled" from day total to clean up UX

--- app.js
+++ app.js
@@ -426,7 +426,7 @@ function renderStaffingGroups(dayKey, dayEvents) {
       </label>
       <div class="planner-row">
         <span class="planner-title">Day total</span>
-        <span class="planner-total"><strong>${totalPhotographers}</strong></span>
+        <span class="planner-total-number">${totalPhotographers}</span>
       </div>
     </div>
   `;
